652e5876aad94449885020f583c196e0213f40c2
NBOCCHINI <nicholasbocchini@gmail.com>
2019-03-10 22:46:41 -0500

    Added simple custum error pages for 404 and 403 errors to app

--- puppyblog/__init__.py
+++ puppyblog/__init__.py
@@ -3,5 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from puppyblog.core.views import core
+from puppyblog.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
