e4467cbbe6d5a27bb14e65cfd94508502a564c7a
NBOCCHINI <nicholasbocchini@gmail.com>
2019-04-13 21:31:22 -0500

    Added flask app secret_key config value to simple string for DEV only testing

--- puppyblog/__init__.py
+++ puppyblog/__init__.py
@@ -6,6 +6,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'mySecretKye'
+
 ##################################
 ###     Database Setup         ###
 ##################################
