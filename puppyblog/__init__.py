d09a3140e31484f6d8bea166302df967096afa17
NBOCCHINI <nicholasbocchini@gmail.com>
2019-04-13 21:17:13 -0500

    Created html template files for the account, login, register, and blog post pages. Created the views to use each template file as well.

--- puppyblog/__init__.py
+++ puppyblog/__init__.py
@@ -25,7 +25,9 @@ login_manager.login_view = 'users.login'
 
 
 from puppyblog.core.views import core
+from puppyblog.users.views import users
 from puppyblog.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
+app.register_blueprint(users)
 app.register_blueprint(error_pages)
