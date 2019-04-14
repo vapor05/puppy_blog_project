f3473004baab4246ed1a59104b87498468481eb5
NBOCCHINI <nicholasbocchini@gmail.com>
2019-04-13 20:21:11 -0500

    Created the forms for the User object so users can login, register an account, and update an existing account. Also create a new script to help handle uploading profile pictures

---
+++ puppyblog/users/forms.py
@@ -0,0 +1,42 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from flask_wtf.file import FileField, FileAllowed
+
+from flask_login import current_users
+from puppyblog.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('UserName', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo=('pass_confirm', message='Passwords must match')])
+    pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('This email has been registered already')
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('This user name has been registered already')
+
+class UpdateUserForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('UserName', validators=[DataRequired()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['png, jpg'])])
+    submit = SubmitField('Update')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('This email has been registered already')
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('This user name has been registered already')
